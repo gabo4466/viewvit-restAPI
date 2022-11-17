9ea2347a7f87dc22de7001f66ec23dc6ce65cbf7
Jaime Romero <romerocal.jaime@gmail.com>
2022-11-17 12:49:23 +0100

    Revert "User resource"
    This reverts commit 6234ca3d9dba2d129e4cc6685407c534a19eb653.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { UserModule } from './user/user.module';
 
 @Module({
-    imports: [ConfigModule.forRoot(), UserModule],
+    imports: [ConfigModule.forRoot()],
     controllers: [],
     providers: [],
 })
